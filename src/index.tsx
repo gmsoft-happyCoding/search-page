e00fd9b744228d826b028138a233b3edbab5b198
angular-moon <xiaoxuss@163.com>
2019-08-19 15:28:18 +0800

    export NO_DATA

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import createSearchPage from './createSearchPage';
 import buildFiltersForm from './filters/buildFiltersForm';
 import FormWrapper from './filters/FormWapper';
+import { NO_DATA } from './useSearchPage/defaultState';
 
 export * from './typing';
-export { buildFiltersForm, FormWrapper };
+export { buildFiltersForm, FormWrapper, NO_DATA };
 export default createSearchPage;
